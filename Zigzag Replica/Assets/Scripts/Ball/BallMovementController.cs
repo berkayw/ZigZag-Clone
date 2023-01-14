b32540504a073c4a1a1c012652bfd2e1ac075cd4
berkayw <berkayw737@gmail.com>
2023-01-14 17:24:06 +0300

    Restart Game Added

--- Zigzag Replica/Assets/Scripts/Ball/BallMovementController.cs
+++ Zigzag Replica/Assets/Scripts/Ball/BallMovementController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallMovementController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class BallMovementController : MonoBehaviour
     [SerializeField] private BallDataTransmitter ballDataTransmitter;
 
     [SerializeField] private float ballMoveSpeed;
+    
 
     private void Update()
     {
@@ -17,6 +19,15 @@ public class BallMovementController : MonoBehaviour
     private void SetBallMovement()
     {
         transform.position += ballDataTransmitter.GetBallDirection() * ballMoveSpeed * Time.deltaTime;
+        restartGame();
     }
 
+
+    private void restartGame()
+    {
+        if (transform.position.y < -6)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
 }
